2fb5af498fe3b3db335586ee3acc175c42d9df78
Stephen Ward <spworwell@gmail.com>
2022-05-28 20:57:30 -0400

    initial commit w/ google & facebook auth working

---
+++ src/PVK.Meetups.Web/Data/PVKMeetupsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PVK.Meetups.Web.Data
+{
+    public class PVKMeetupsDbContext : IdentityDbContext
+    {
+        public PVKMeetupsDbContext(DbContextOptions<PVKMeetupsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/PVK.Meetups.Web/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PVK.Meetups.Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// configure forwarded headers so the ap works behind a load balancer
+builder.Services.Configure<ForwardedHeadersOptions>(options => {
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("LocalPostgresConnection");
+builder.Services.AddDbContext<PVKMeetupsDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+})
+.AddEntityFrameworkStores<PVKMeetupsDbContext>();
+
+
+var config = builder.Configuration;
+builder.Services.AddAuthentication()
+        .AddGoogle(options =>
+        {
+            options.ClientId = config.GetSection("Authentication").GetValue<string>("GoogleClientId");
+            options.ClientSecret = config.GetSection("Authentication").GetValue<string>("GoogleClientSecret");
+            options.AccessDeniedPath = "/AccessDeniedPathInfo";
+        })
+        .AddFacebook(options =>
+        {
+            options.ClientId = config.GetSection("Authentication").GetValue<string>("FacebookClientId");
+            options.ClientSecret = config.GetSection("Authentication").GetValue<string>("FacebookClientSecret");
+            options.AccessDeniedPath = "/AccessDeniedPathInfo";
+        })
+        ;
+
+
+builder.Services.AddControllersWithViews();
+
+
+
+
+
+
+var app = builder.Build();
+
+app.UseForwardedHeaders();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
