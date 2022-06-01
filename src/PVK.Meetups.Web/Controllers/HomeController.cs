fc60e465b2b060a821e64788a9e601a0831c802b
Stephen Ward <spworwell@gmail.com>
2022-05-31 23:16:56 -0400

    added a Terms of Use page

--- src/PVK.Meetups.Web/Controllers/HomeController.cs
+++ src/PVK.Meetups.Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace PVK.Meetups.Web.Controllers
         {
             return View();
         }
+        public IActionResult TermsOfUse()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
